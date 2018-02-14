dc3f6dafbe2e58ec6ee1d8bb4571a85b58a75e4d
StephenDK <stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.com>
2018-02-13 20:35:14 -0800

    html survey route

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,16 @@
+// Dependencies
+// 
+var path = require("path");
+
+// Routing
+// 
+module.exports = function(application) {
+
+    // Path redirects
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "/../public/survey.html"));
+
+    })
+
+    
+}
