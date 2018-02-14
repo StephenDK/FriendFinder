ddf083faa59afbdff884e8386709428f34b90a87
StephenDK <stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.comgit config --global user.email stephendklein08@gmail.com>
2018-02-13 20:16:22 -0800

    Form validation

--- server.js
+++ server.js
@@ -5,3 +5,25 @@
  var express = require("express");
 //  bodyParser
 var bodyParser = require("body-parser");
+
+// Create an instance of express
+var app = express();
+
+// Set initial port
+var PORT = process.env.PORT || 3000;
+
+// Set up body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// require api routes
+
+
+
+// Listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+})
+
+
